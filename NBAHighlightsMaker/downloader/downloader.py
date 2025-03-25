2742fd66c38e5d4fb0993ae553c074fa679de771
etnguyen401 <nguyeneric280@gmail.com>
2025-03-24 23:55:08 -0700

    finish editng module

--- NBAHighlightsMaker/downloader/downloader.py
+++ NBAHighlightsMaker/downloader/downloader.py
@@ -1,8 +1,6 @@
 # import sys
 import os
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 #use zendriver to act more like a human
 import asyncio
 import random
@@ -15,16 +13,13 @@ from NBAHighlightsMaker.players.getplayers import read_event_ids
 #into the data folder
 #IN FUTURE, LET USER SPECIFY WHERE TO DOWNLOAD?
 async def download_links(event_ids):
-    config = zd.Config()
-    config.headless = True
-    config.add_argument = "--mute-audio"
     data_dir = os.path.join(os.getcwd(), 'data', 'vids')
     os.makedirs(data_dir, exist_ok=True)
-
+    # make new column in event ids to store file path
     for index, row in event_ids.iterrows():
         desc = row['DESCRIPTION']
         dl_link = row['VIDEO_LINK']    
-
+        event_num = row['EVENTNUM']
         #start browser for each link
         browser = await zd.start(headless=True, 
                                 browser_args=["--mute-audio"])
@@ -39,17 +34,47 @@ async def download_links(event_ids):
         # download to correct path (might need to get 
         # another WCTIMESTRING column
         # for better names)
-        await tab.download_file(dl_link)
+        await tab.download_file(dl_link, "{}.mp4".format(event_num))
         #sleep for random time
         print("Sleeping after download of {}...".format(desc))
         await asyncio.sleep(random.uniform(2.0, 4.0))
 
         await browser.stop()
 
+async def test_download():
+    data_dir = os.path.join(os.getcwd(), 'data', 'test')
+    os.makedirs(data_dir, exist_ok=True)
+    url = 'https://videos.nba.com/nba/pbp/media/2025/03/04/0022400889/14/bd1edcac-5099-e0de-053c-2c6d71808c05_1280x720.mp4'
+
+    #start browser for each link
+    browser = await zd.start(headless=True, 
+                            browser_args=["--mute-audio"])
+    
+    # go to link
+    tab = await browser.get(url)
+    await tab.set_download_path(data_dir)
+    # wait for page to load
+    await tab
+    print("Sleeping before download of {}...".format('test'))
+    await asyncio.sleep(random.uniform(0.5, 1.5))
+    # download to correct path (might need to get 
+    # another WCTIMESTRING column
+    # for better names)
+    test = await tab.download_file(url, 'test.mp4')
+    print("Downloaded file: ", test)
+    #sleep for random time
+    print("Sleeping after download of {}...".format('test'))
+    await asyncio.sleep(random.uniform(2.0, 4.0))
+
+    await browser.stop()
+
 def main():
     # read events id file (ADD FUNCTIONALITY TO MAKE SURE IT HAS THE LINKS, NOT JUST EVENT IDS)
     event_ids = read_event_ids()
     asyncio.run(download_links(event_ids))
+
+    #asyncio.run(test_download())
+
 
 if __name__ == '__main__':
     main()
---
+++ NBAHighlightsMaker/editor/editor.py
@@ -0,0 +1,70 @@
+from moviepy import *
+import os
+from NBAHighlightsMaker.players.getplayers import read_event_ids
+
+#organize files in video created date
+#MIGHT CHANGE TO ORGANIZE BY COMBINATION OF REAL LIFE TIME AND PERIOD TIME 
+#REMAINING
+#OR WHEN CLIP IS DOWNLOADED, COMBINE PATH AND DATA DIRECTORY, ADD TO ROW
+
+def get_clip_order(data_dir):
+    #get all files in data directory
+    clips_paths = [f for f in os.listdir(data_dir) if f.endswith('.mp4')]
+    sorted_clips_paths = sorted(clips_paths, key=lambda x: int(x.split('.')[0]))
+    return sorted_clips_paths
+
+# given a videoFileClip from moviepy, make a text overlay and return it
+def create_overlay(string):
+    txt_clip = TextClip(font= './resources/Boldonse-Regular.ttf',
+                                text = string, 
+                                font_size = 50, 
+                                color = 'white', 
+                                text_align = 'center',
+                                duration = 3,
+    )
+    #txt_clip = txt_clip.with_position(('bottom', 'right'))
+    return txt_clip
+
+def create_video_clip(clip_path):
+    clip = VideoFileClip(clip_path, target_resolution= (1280, 720))
+    return clip
+
+# make each clip into a videoFileClip, and call create overlay to get the text clip
+# then make a composite videoFileClip
+def create_composite_clips(clips_paths, data_dir):
+    #get pd dataframe of the descriptions for each clip
+    event_ids = read_event_ids()
+    composite_clips = []
+    #for each clip path
+    for clip_path in clips_paths:
+        event_num = int(clip_path.split('.')[0])
+        full_clip_path = os.path.join(data_dir, clip_path)
+        desc = event_ids.loc[event_ids['EVENTNUM'] == event_num, 'DESCRIPTION'].values[0]
+        #make into videoClip
+        clip = create_video_clip(full_clip_path)
+        #make text overlay
+        txt_clip = create_overlay(desc)
+        #make it a composite video
+        composite_clip = CompositeVideoClip([clip, txt_clip])
+        composite_clips.append(composite_clip)
+    
+    return composite_clips
+
+# concatenate all composite clips
+def make_final_vid(data_dir, composite_clips):
+    final_vid = concatenate_videoclips(composite_clips)
+    path = os.path.join(data_dir, "final_vid.mp4")
+    final_vid.write_videofile(path, codec = 'libx264', fps = 24)
+    return final_vid
+
+def main():
+    data_dir = os.path.join(os.getcwd(), 'data', 'vids')
+    clip_paths = get_clip_order(data_dir)
+    composite_clips = create_composite_clips(clip_paths, data_dir)
+    final_vid = make_final_vid(data_dir, composite_clips)
+    # print(clip_order)
+    # test = create_overlay("test")
+
+
+if __name__ == '__main__':
+    main()
