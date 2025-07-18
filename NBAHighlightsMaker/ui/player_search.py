6c713a50d815de9c660eda614c16c8031680e573
etnguyen401 <nguyeneric280@gmail.com>
2025-07-17 21:59:52 -0700

    add current year to selection

--- NBAHighlightsMaker/ui/player_search.py
+++ NBAHighlightsMaker/ui/player_search.py
@@ -26,11 +26,11 @@ class PlayerSearchBox(QWidget):
 
         # box to select season
         self.season_box = QComboBox(self)
-        self.season_box.addItems(["2024-25", "2023-24", "2022-23", 
-                                  "2021-22", "2020-21", "2019-20",
-                                  "2018-19", "2017-18", "2016-17",
-                                  "2015-16", "2014-15", "2013-14",
-                                  "2012-13"])
+        self.season_box.addItems(["2025-26", "2024-25", "2023-24", 
+                                  "2022-23", "2021-22", "2020-21", 
+                                  "2019-20", "2018-19", "2017-18", 
+                                  "2016-17", "2015-16", "2014-15", 
+                                  "2013-14", "2012-13"])
         # box to select season type
         self.season_type_box = QComboBox(self)
         self.season_type_box.addItems(["Regular Season", "Playoffs",
